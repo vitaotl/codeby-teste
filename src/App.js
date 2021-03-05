4f4f65f176b7ff44c16ad0dac435027f7dbf29ac
victor <victor.htp@hotmail.com>
2021-03-05 11:15:55 -0300

    add json files

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import Switch from '@material-ui/core/Switch';
 
+import abaixoDeDez from './api/abaixoDeDez.json'
+import acimaDeDez from './api/acimaDeDez.json'
 import './App.css';
 import Product from './components/Product'
 
 function App() {
-  let abaixoDeDezUrl = 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/5bbd6fdd-abae-411d-96cc-1a5d76d3803b/abaixo-10-reais.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20210302%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210302T143819Z&X-Amz-Expires=86400&X-Amz-Signature=2d5c466fe76b4060873661f95bf1481c201939205dbd80fc5a7260f89f8b7b49&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22abaixo-10-reais.json%22'
-  let acimaDeDezUrl = 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/11b895d0-bc64-4f3a-bfa9-7c652be8d415/acima-10-reais.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20210302%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210302T163708Z&X-Amz-Expires=86400&X-Amz-Signature=297112925541b87f082ad43cf8a99ab3e695e4ca4bb94891ea722118ca31818c&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22acima-10-reais.json%22'
+  let abaixoDeDezUrl = 'api/abaixoDeDez.json' 
+  let acimaDeDezUrl = 'api/acimaDeDez.json'
   const [products, setProducts] = useState([])
   const [total, setTotal] = useState(0)
   const [desconto, setDesconto] = useState(0)
@@ -21,11 +23,12 @@ function App() {
   useEffect(() => {
     axios.get(url)
       .then(res => {
-        setProducts(res.data)
-        let total = (res.data.totalizers[0].value / 100)
-        let desconto = (res.data.totalizers[1].value / 100)
+        setProducts(res)
+        let total = (res.totalizers[0].value / 100)
+        let desconto = (res.totalizers[1].value / 100)
         setTotal(total)
         setDesconto(desconto)
+        console.log(res)
       })
       .catch(err => console.log(err))
 
